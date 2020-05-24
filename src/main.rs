f44a160998ef4141cc01e731c2b15d811d2c8413
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-05-24 20:57:39 +0900

    #2 コマンドラインインターフェースの大枠完成

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,66 @@
+use clap::Clap;
 
-
-struct Cli {
-    sub_command: String,
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Takumi Okamoto <takumi1988okamoto@gmail.com>")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
 }
 
-impl Cli {
-    fn from_args() -> Cli{
-        let pattern = std::env::args().nth(1).expect("no sub command given");
-        let path = std::env::args().nth(2).expect("no pattern given");
-        Cli{
-            sub_command: pattern,
-        }
-    }
+#[derive(Clap)]
+enum SubCommand {
+    #[clap()]
+    SendScript(SendScript),
+    #[clap()]
+    Ds(DashboardServer),
 }
+
+
+#[derive(Clap,Debug)]
+struct SendScript{
+    input: String,
+    input2: String,
+}
+
+#[derive(Clap)]
+#[clap()]
+struct DashboardServer{
+    #[clap(subcommand)]
+    subsubcmd: DashboardServerSubCommand,
+}
+
+#[derive(Clap)]
+enum DashboardServerSubCommand{
+    #[clap()]
+    Play(DSPlay),
+    #[clap()]
+    Load(DSLoad),
+}
+
+#[derive(Clap,Debug)]
+struct DSPlay {
+    address: String,
+}
+
+#[derive(Clap,Debug)]
+struct DSLoad {
+    address: String,
+    file_name: String,
+}
+
 
 fn main() {
-    let args = Cli::from_args();
+    let opts: Opts = Opts::parse();
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::SendScript(t) => println!("{:?}",t),
+        SubCommand::Ds(subcmd) => match subcmd.subsubcmd {
+            DashboardServerSubCommand::Load(l) => println!("{:?}",l),
+            DashboardServerSubCommand::Play(l) => println!("{:?}",l,),
+        }
+    }
+
+    // more program logic goes here...
 }
-
-
